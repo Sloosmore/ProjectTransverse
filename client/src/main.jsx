9037ee8990bb4fbe5492ba1cf18aa5209bb81333
Sloosmore <loosmore@usc.edu>
2024-02-04 09:20:00 -0800

    Update import in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import TransverseApp from "./tvApp.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
