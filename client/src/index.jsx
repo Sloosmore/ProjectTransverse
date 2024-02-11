8cbc12919d7292884d5d4548749ca0ba9afa94c7
Sloosmore <loosmore@usc.edu>
2024-02-11 13:54:30 -0800

    Update user authentication logic

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import TransverseApp from "./componets/appPages/tvApp.jsx";
 //import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.css";
 
