fd47cd53cba18aa07a2560a852d2b980e4276aaf
Sloosmore <loosmore@usc.edu>
2024-02-12 11:55:46 -0800

    Update puppeteer launch options and add request logs

--- express_server/controllers/handleNoteDownload.js
+++ express_server/controllers/handleNoteDownload.js
@@ -38,7 +38,9 @@ const downloadNote = async (req, res) => {
       // Inject CSS into HTML
 
       // Convert HTML to PDF
-      const browser = await puppeteer.launch();
+      const browser = await puppeteer.launch({
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      });
       const page = await browser.newPage();
       await page.setContent(html);
       await page.addStyleTag({
