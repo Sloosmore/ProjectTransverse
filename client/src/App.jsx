2943ce248d0f768404db704876e46ad3f1f86df6
Sloosmore <loosmore@usc.edu>
2024-02-20 08:15:58 -0800

    Add @vercel/analytics and @vercel/speed-insights packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,24 +5,30 @@ import PublicRoutes from "./components/publicPages/publicRouter.jsx";
 import AuthProvider from "./hooks/auth.jsx";
 import ProtectedRoute from "./components/appPages/protectedRoute.jsx";
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route
-            path="/n/*"
-            element={
-              <ProtectedRoute>
-                <TransverseApp />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/*" element={<PublicRoutes />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route
+              path="/n/*"
+              element={
+                <ProtectedRoute>
+                  <TransverseApp />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/*" element={<PublicRoutes />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+      <SpeedInsights />
+      <Analytics />
+    </>
   );
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,5 @@
 export default {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
