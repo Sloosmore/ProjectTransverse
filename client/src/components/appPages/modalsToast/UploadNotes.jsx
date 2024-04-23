8fd1b5857bbf3406445ab30d2310f1875cbcea64
Sloosmore <loosmore@usc.edu>
2024-04-23 09:03:29 -0700

    Refactor file input and button layout in UploadNotes component

--- client/src/components/appPages/modalsToast/UploadNotes.jsx
+++ client/src/components/appPages/modalsToast/UploadNotes.jsx
@@ -65,28 +65,30 @@ const UploadNotes = ({ activeNum, setPreferences, setTextareaValue }) => {
   return (
     <div className="pb-6 border-b">
       <label
-        className="block text-gray-400 dark:text-white mb-2"
+        className="block text-gray-400 dark:text-white mb-2 "
         htmlFor="file_input"
       >
         Upload Your own notes (PDF)
       </label>
-      <input
-        type="file"
-        onChange={handleFileChange}
-        accept=".pdf"
-        className="text-sm relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-gray-400 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
-      />
-      <Button
-        onClick={() => {
-          if (file) {
-            handleFileUpload(file, activeNum, session, setTextareaValue);
-          }
-        }}
-        variant="secondary"
-        className="bg-gray-200 mt-4"
-      >
-        Generate Preferences
-      </Button>
+      <div className="flex flex-row">
+        <input
+          type="file"
+          onChange={handleFileChange}
+          accept=".pdf"
+          className="me-5 text-sm relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[.58rem] text-gray-400 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.58rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.58rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+        />
+        <Button
+          onClick={() => {
+            if (file) {
+              handleFileUpload(file, activeNum, session, setTextareaValue);
+            }
+          }}
+          variant="secondary"
+          className="bg-gray-200"
+        >
+          Generate Preferences
+        </Button>
+      </div>
     </div>
   );
 };
